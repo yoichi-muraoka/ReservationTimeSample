29395168917227814c9e29a358c5cd17d9ff13a6
Muraoka <muraoka@villhill.net>
2022-04-21 10:49:04 +0900

    init

---
+++ src/main/Application.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// 現在の予約状況を表示
+		System.out.println("[現在の予約状況]--------------");
+		System.out.println("10:00～12:00　笹川紘一様");
+		System.out.println("15:00～16:30　本宮弘子様");
+		System.out.println("18:30～20:30　藤上達也様");
+		System.out.println("------------------------------\n");
+		
+		// 名前、利用開始時刻、終了時刻を入力してもらう
+		System.out.println("お名前、利用開始時刻、終了時刻を入力してください。");
+		System.out.println("8:00～22:00の間で予約を受け付けています。\n");
+		System.out.println("お名前　 >>> 山田太郎");
+		System.out.println("開始時刻 >>> 14:00");
+		System.out.println("終了時刻 >>> 15:00");
+		
+		// 入力時刻のバリデーション
+		System.out.println("\n入力、ありがとうございました。ご予約可能か確認します。\n");
+		System.out.println("... 確認中 ...\n");
+		
+		// 予約受付の完了、または予約できない旨を表示
+		System.out.println("14:00～15:00で、山田太郎様のご予約を承りました。");
+		System.out.println("申し訳ありませんが、ご希望の時間では予約できません。");
+	}
+
+}
