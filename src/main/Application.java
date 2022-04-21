b69be2e9c3726ba2f40c5287b93656e00a031338
Muraoka <muraoka@villhill.net>
2022-04-21 14:30:01 +0900

    入力値の取得と時刻のバリデーション

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,11 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
+import domain.Reservation;
 import service.ReservationService;
 
 public class Application {
 
 	public static void main(String[] args) {
 		ReservationService service = new ReservationService();
+		Scanner scanner = new Scanner(System.in);
 		
 		// 現在の予約状況を表示
 		System.out.println("[現在の予約状況]--------------");
@@ -15,17 +19,31 @@ public class Application {
 		// 名前、利用開始時刻、終了時刻を入力してもらう
 		System.out.println("お名前、利用開始時刻、終了時刻を入力してください。");
 		System.out.println("8:00～22:00の間で予約を受け付けています。\n");
-		System.out.println("お名前　 >>> 山田太郎");
-		System.out.println("開始時刻 >>> 14:00");
-		System.out.println("終了時刻 >>> 15:00");
+		
+		System.out.print("お名前　 >>> ");
+		String name = scanner.nextLine();
+		
+		System.out.print("開始時刻 >>> ");
+		String from = scanner.nextLine();
+		
+		System.out.print("終了時刻 >>> ");
+		String to = scanner.nextLine();
+		
+		// 予約オブジェクトの生成
+		Reservation reservationToAdd = new Reservation(name, from, to);
 		
 		// 入力時刻のバリデーション
 		System.out.println("\n入力、ありがとうございました。ご予約可能か確認します。\n");
 		System.out.println("... 確認中 ...\n");
+		// 予約受付の完了、または予約できない旨を表示		
+		if(service.isAbleToReserve(reservationToAdd)) {
+			System.out.println(from + "～" + to + "で、" + name + "様のご予約を承りました。");
+			// TODO データベースに保存する等の処理
+		} else {
+			System.out.println("申し訳ありませんが、ご希望の時間では予約できません。");
+		}
 		
-		// 予約受付の完了、または予約できない旨を表示
-		System.out.println("14:00～15:00で、山田太郎様のご予約を承りました。");
-		System.out.println("申し訳ありませんが、ご希望の時間では予約できません。");
+		scanner.close();
 	}
 
 }
